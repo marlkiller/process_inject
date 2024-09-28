4d4ffafceac04c626f5b1b40e8b708db7f290327
Void <marlkiller@voidm.com>
2024-09-28 17:58:23 +0800

    Initial Commit

---
+++ process_inject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  process_inject
+//
+//  Created by 马治武 on 2024/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
